39fae0407ab1ed3a113c1cd86941d279d8656bab
刘兵 <17734063743@163.com>
2020-05-04 16:31:31 +0800

    123

--- src/main/java/com/eric/hchat/pojo/vo/Test.java
+++
@@ -1,8 +0,0 @@
-package com.eric.hchat.pojo.vo;
-
-public class Test {
-    public static void main(String[] args) {
-        User u = User.builder().id("123").username("士大夫").build();
-        System.out.println(u);
-    }
-}
